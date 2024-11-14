a1afafcc423d250f5ccf78da9a7af460b156af3d
azize1999 <azizchatti00@gmail.com>
2024-11-14 05:11:16 +0100

    correction code

--- src/components/Select/index.test.js
+++ src/components/Select/index.test.js
@@ -3,7 +3,7 @@ import Select from "./index";
 
 describe("Select Component", () => {
   
-  it("displays a list of choices with a default option 'Toutes'", () => {
+  it("displays a list of choices with a default option 'Toutes'", () => { 
     render(<Select selection={["value1", "value2"]} />);
     
     const selectElement = screen.getByTestId("select-testid");
